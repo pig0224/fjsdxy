65609322592ff387630f7a6229bce89d07cd5a56
小猪 <xiaomo_0258@icloud.com>
2019-08-17 19:47:23 +0800

    改用go mod依赖管理

--- sy.go
+++ sy.go
@@ -30,7 +30,9 @@ func SSO_Login(studentID, password string) (*colly.Collector, error) {
 	c.OnHTML("#msg", func(e *colly.HTMLElement) {
 		logErr = errors.New(e.Text)
 	})
+	c.OnResponse(func(response *colly.Response) {
 
+	})
 	err := c.Post(config.CAS_DOMAIN+"/cas/login", map[string]string{
 		"username":   loginForm.studentId,
 		"password":   loginForm.password,
